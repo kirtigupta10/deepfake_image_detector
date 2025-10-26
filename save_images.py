1af774b087cf27ed36b9b3a8ca9eca4670b702a1
lisaghimire12 <lisa.ghimire1@gmail.com>
2025-10-26 23:21:19 +0530

    Added datasets

--- save_images.py
+++ save_images.py
@@ -1,19 +1,15 @@
-# save_images.py
 from datasets import load_dataset
 import os
 from tqdm import tqdm
 
-# Load dataset
 dataset = load_dataset("JamieWithofs/Deepfake-and-real-images", split="train")
 
-# Create folders for saving
 os.makedirs("data/real", exist_ok=True)
 os.makedirs("data/fake", exist_ok=True)
 
-# Save first 500 images (for testing)
 for i, item in enumerate(tqdm(dataset)):
     label = 'real' if item['label'] == 0 else 'fake'
-    img = item['image']  # PIL Image object
+    img = item['image']  
     img_path = f"data/{label}/{i}.jpg"
 
     try:
--- verify_images.py
+++ verify_images.py
@@ -2,11 +2,9 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import os
 
-# Choose one from each folder
 real_img_path = os.path.join("data", "real", os.listdir("data/real")[0])
 fake_img_path = os.path.join("data", "fake", os.listdir("data/fake")[0])
 
-# Load and show images
 real_img = Image.open(real_img_path)
 fake_img = Image.open(fake_img_path)
 
